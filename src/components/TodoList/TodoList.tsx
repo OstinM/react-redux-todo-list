10fd04c1e614cba2cb548ce2fc55f365e04c7e6f
Malyholovka Ostap <maligolovkaostap@gmail.com>
2024-04-20 19:50:44 +0300

    removed extra code

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -15,7 +15,6 @@ export const USER_ID = 1;
 export const TodoList = () => {
   const todos = useAppSelector((state) => state.todos);
   const dispatch = useAppDispatch();
-  console.log(todos);
 
   const handleDragEnd = (result: DropResult) => {
     const { destination, source } = result;
